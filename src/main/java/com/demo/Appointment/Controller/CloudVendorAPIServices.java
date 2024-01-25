893bd793f1d1dce62ed278eaa1e9ec3aed16518f
BreachPearl <geolight03@gmail.com>
2024-01-24 19:46:20 -0500

    Update CloudVendorAPIServices.java
    Added Putmapping  to update the values and added the deletemapping to delete the values using the user id

--- src/main/java/com/demo/Appointment/Controller/CloudVendorAPIServices.java
+++ src/main/java/com/demo/Appointment/Controller/CloudVendorAPIServices.java
@@ -1,7 +1,9 @@
 package com.demo.Appointment.Controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,17 @@ public class CloudVendorAPIServices {
     public String createVendorDetails(@RequestBody CloudVendor cloudven) {
         this.cloudven = cloudven;
         return "data is inserted Successfully";
+    }
+
+    @PutMapping
+    public String updateVendorDetails(@RequestBody CloudVendor cloudven) {
+        this.cloudven = cloudven;
+        return "data is updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteVendorDetails(String vendorId) {
+        this.cloudven = null;
+        return "data is deleted Successfully";
     }
 }
