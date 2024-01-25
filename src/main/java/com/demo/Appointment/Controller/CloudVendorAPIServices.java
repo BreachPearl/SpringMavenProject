ed5464ccc3307a58ac7d5379d39df463a871a397
BreachPearl <geolight03@gmail.com>
2024-01-24 19:22:58 -0500

    Update CloudVendorAPIServices.java
    added the post mapping Annotation to create new vendors

--- src/main/java/com/demo/Appointment/Controller/CloudVendorAPIServices.java
+++ src/main/java/com/demo/Appointment/Controller/CloudVendorAPIServices.java
@@ -1,6 +1,8 @@
 package com.demo.Appointment.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.demo.Appointment.Model.CloudVendor;
@@ -8,8 +10,16 @@ import com.demo.Appointment.Model.CloudVendor;
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIServices {
+    CloudVendor cloudven;
+
     @GetMapping("{vendorId}")
     public CloudVendor getCloudVendorDetails(String vendorID) {
-        return new CloudVendor("1", "Sumanth", "4707", "301xxxx");
+        return cloudven;
+    }
+
+    @PostMapping
+    public String createVendorDetails(@RequestBody CloudVendor cloudven) {
+        this.cloudven = cloudven;
+        return "data is inserted Successfully";
     }
 }
