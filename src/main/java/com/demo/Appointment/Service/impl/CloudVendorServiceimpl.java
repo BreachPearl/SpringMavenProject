74972b9f53410a5556b43b93df0c07684e216009
BreachPearl <geolight03@gmail.com>
2024-01-28 12:22:14 -0500

    Create CloudVendorServiceimpl.java
    Added the Service layer implementation like writing the method implementations

---
+++ src/main/java/com/demo/Appointment/Service/impl/CloudVendorServiceimpl.java
@@ -0,0 +1,45 @@
+package com.demo.Appointment.Service.impl;
+
+import java.util.List;
+
+import com.demo.Appointment.Model.CloudVendor;
+import com.demo.Appointment.Service.CloudVendorService;
+import com.demo.Appointment.repository.CloudVendorRepository;
+
+public class CloudVendorServiceimpl implements CloudVendorService {
+
+    CloudVendorRepository cloudVendorRepository;
+
+    public CloudVendorServiceimpl(CloudVendorRepository cloudVendorRepository) {
+        this.cloudVendorRepository = cloudVendorRepository;
+    }
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudvendor) {
+        cloudVendorRepository.save(cloudvendor);
+        return "Success";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudvendorId) {
+        cloudVendorRepository.deleteById(cloudvendorId);
+        return "Success";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudvedorId) {
+        return cloudVendorRepository.findById(cloudvedorId).get();
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendors() {
+        return cloudVendorRepository.findAll();
+    }
+
+}
