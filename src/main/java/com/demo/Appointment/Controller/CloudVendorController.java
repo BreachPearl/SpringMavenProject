0ed72116930b3cd5eaff3c3093d095f6bd810ccf
BreachPearl <geolight03@gmail.com>
2024-01-28 12:58:03 -0500

    Update CloudVendorController.java
    connected with the service layer

--- src/main/java/com/demo/Appointment/Controller/CloudVendorController.java
+++ src/main/java/com/demo/Appointment/Controller/CloudVendorController.java
@@ -1,39 +1,54 @@
 package com.demo.Appointment.Controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.demo.Appointment.Model.CloudVendor;
+import com.demo.Appointment.Service.CloudVendorService;
 
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorController {
-    CloudVendor cloudven;
+    CloudVendorService cloudVendorService;
+
+    public CloudVendorController(CloudVendorService cloudVendorService) {
+        this.cloudVendorService = cloudVendorService;
+    }
 
     @GetMapping("{vendorId}")
-    public CloudVendor getCloudVendorDetails(String vendorID) {
-        return cloudven;
+    public CloudVendor getCloudVendorDetails(@PathVariable("vendorId") String vendorID) {
+        return cloudVendorService.getCloudVendor(vendorID);
+
+    }
+
+    @GetMapping
+    public List<CloudVendor> getAllCloudVendorDetails() {
+        return cloudVendorService.getAllCloudVendors();
+
     }
 
     @PostMapping
-    public String createVendorDetails(@RequestBody CloudVendor cloudven) {
-        this.cloudven = cloudven;
-        return "data is inserted Successfully";
+    public String createVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        cloudVendorService.createCloudVendor(cloudVendor);
+        return "cloudvendor is created Successfully";
     }
 
     @PutMapping
-    public String updateVendorDetails(@RequestBody CloudVendor cloudven) {
-        this.cloudven = cloudven;
-        return "data is updated Successfully";
+    public String updateVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        cloudVendorService.updateCloudVendor(cloudVendor);
+        return "cloudvendor is updated Successfully";
     }
 
     @DeleteMapping("{vendorId}")
-    public String deleteVendorDetails(String vendorId) {
-        this.cloudven = null;
-        return "data is deleted Successfully";
+    public String deleteVendorDetails(@PathVariable("vendorId") String vendorId) {
+        cloudVendorService.deleteCloudVendor(vendorId);
+        return "cloudvendor is deleted Successfully";
     }
 }
